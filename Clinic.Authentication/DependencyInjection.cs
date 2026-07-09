17ffb4f1bbdc83f84eed087237edf8b36a65d109
Abdelrahman Almowafy <abdoalmowafy@gmail.com>
2026-07-09 19:43:38 +0300

    feat: configure JwtBearerEvents to extract access tokens from query strings for SignalR hubs

--- Clinic.Authentication/DependencyInjection.cs
+++ Clinic.Authentication/DependencyInjection.cs
@@ -46,6 +46,21 @@ public static class DependencyInjection
                 ClockSkew = TimeSpan.Zero,
                 RoleClaimType = "roles"
             };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                    {
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
         });
 
         // Add authorization policies
